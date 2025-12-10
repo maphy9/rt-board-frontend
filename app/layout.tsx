991a9b1e5cafbcfb06cf7b0ac5759145d104a6c1
maphy9 <maphy9@gmail.com>
2025-12-10 23:28:22 +0100

    feat: Add layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,17 @@
+import type { Metadata } from "next";
+import React, { ReactNode } from "react";
+
+export const metadata: Metadata = {
+  title: "React App",
+  description: "Web site created with Next.js.",
+};
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        <div id="root">{children}</div>
+      </body>
+    </html>
+  );
+}
